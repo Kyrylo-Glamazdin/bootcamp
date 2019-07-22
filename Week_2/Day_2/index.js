cb3cb6907ea980445b875bbeded86c3f424d272a
Kyrylo Glamazdin <kyrylo.glamazdin36@myhunter.cuny.edu>
2019-07-22 19:56:32 -0400

    adding index js

---
+++ Week_2/Day_2/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import ContactCard from './ContactCard';
+import Decrement from './Decrement';
+
+ReactDOM.render(
+	// <div people>
+ //  		<ContactCard name="Tom" mobileNum="+1(123)456-7890" email="tom@tomMail.com" workNum="+1(121)454-6776"/>
+ //  		<ContactCard name="Julia" mobileNum="+1(908)765-4321" email="julia@juliaMail.com"/>
+ //  		<ContactCard name = "Alex" mobileNum="+1(173)816-941" email="alex@alexMail.com" workNum="+1(573)273-6891"/>
+ //  	</div>,
+ <div>
+ 	<Decrement num="10"/>
+ </div>,
+  document.getElementById('root')
+);
