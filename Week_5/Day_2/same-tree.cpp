e8c2125f57f758fe7503af927d96dd7363932838
Kyrylo Glamazdin <Kyrylo.Glamazdin36@myhunter.cuny.edu>
2019-08-12 22:12:37 -0400

    same-tree solution

---
+++ Week_5/Day_2/same-tree.cpp
@@ -0,0 +1,20 @@
+//Kyrylo Glamazdin
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == q && p == nullptr){
+            return true;
+        }
+        if (p == nullptr || q == nullptr){
+            return false;
+        }
+        if (p->val != q->val){
+            return false;
+        }
+        if (p->val == q->val){
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+        return false;
+    }
+};
